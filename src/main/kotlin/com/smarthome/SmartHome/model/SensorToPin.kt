7a89f7b772f1b34787a386d4cc147cb3c27b45b1
itche <itcherry97@gmail.com>
2019-05-28 23:25:59 +0300

    Changed pins to support real device.

--- src/main/kotlin/com/smarthome/SmartHome/model/SensorToPin.kt
+++ src/main/kotlin/com/smarthome/SmartHome/model/SensorToPin.kt
@@ -3,9 +3,9 @@ package com.smarthome.SmartHome.model
 enum class SensorToPin(val pin: Pin, val direction: PinDirection) {
     CORRIDOR_LIGHT_OUTPUT(Pin.GPIO_14, PinDirection.OUT),
     CORRIDOR_LIGHT_INPUT(Pin.GPIO_17, PinDirection.IN),
-    KITCHEN_LIGHT_OUTPUT(Pin.GPIO_15, PinDirection.OUT),
+    KITCHEN_LIGHT_OUTPUT(Pin.GPIO_21, PinDirection.OUT),
     KITCHEN_LIGHT_INPUT(Pin.GPIO_27, PinDirection.IN),
-    LIVING_ROOM_LIGHT_OUTPUT(Pin.GPIO_21, PinDirection.OUT),
+    LIVING_ROOM_LIGHT_OUTPUT(Pin.GPIO_15, PinDirection.OUT),
     LIVING_ROOM_LIGHT_INPUT(Pin.GPIO_03, PinDirection.IN),
     BEDROOM_LIGHT_OUTPUT(Pin.GPIO_16, PinDirection.OUT),
     BEDROOM_LIGHT_INPUT(Pin.GPIO_22, PinDirection.IN),
